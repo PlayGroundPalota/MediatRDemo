0c08715bff9e6e1eb629ff0eb053670539edc715
buyanimhlongo@gmail.com <buyanimhlongo@gmail.com>
2023-05-10 11:03:11 +0200

    created something to revert

---
+++ SingleSignOnRefactor/Helpers/DateConverterHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SingleSignOnRefactor.Helpers
+{
+    public class DateConverterHelper
+    {
+        public string HelloWorld()
+        {
+            return "Hello World we are in Nkandla";
+        }
+    }
+}
+
---
+++ SingleSignOnRefactor/Helpers/ValidationResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SingleSignOnRefactor.Helpers
+{
+    public class ValidationResult : Results, IValidationResult
+    {
+        public ValidationResult(Err)
+        {
+        }
+
+        public Error[] Errors => throw new NotImplementedException();
+    }
+}
+
